328fba64e940dc0045002f781063fba462cfc773
lolpopami2354 <cyborgv43214@gmail.com>
2025-10-02 16:56:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 <script>
   const playButton = document.getElementById('playButton');
   const gameFrame = document.getElementById('gameFrame');
-  const gameUrl = "https://html5.gamedistribution.com/3b79a8537ebc414fb4f9672a9b8c68c8/?gd_sdk_referrer_url=https://www.example.com/games"; // Replace with the actual URL from GameDistribution
+  const gameUrl = "https://https://www.hoodamath.com/games/snowrider3d.html"; // Replace with the actual URL from GameDistribution
 
   playButton.addEventListener('click', () => {
     gameFrame.src = gameUrl;
